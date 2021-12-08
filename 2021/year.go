01bb7ee2d2fe882821f52078d3c54d0571f2bfc8
mrm <matthew.munton@starleaf.com>
2021-12-08 08:27:53 +0000

    2021 day 8

--- 2021/year.go
+++ 2021/year.go
@@ -11,6 +11,7 @@ import (
 	"adventofcode/2021/05"
 	"adventofcode/2021/06"
 	"adventofcode/2021/07"
+	"adventofcode/2021/08"
 )
 
 func Run(day, inputFile string) {
@@ -30,6 +31,8 @@ func Run(day, inputFile string) {
 		f = day6.Run
 	case "7":
 		f = day7.Run
+	case "8":
+		f = day8.Run
 	default:
 		log.Fatal(day, " is not a valid day")
 	}
